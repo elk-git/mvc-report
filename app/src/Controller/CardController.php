fcd7ffda07c0557d4612bb180499092fce834cf8
Elis Sandklo <elsd23@student.bth.se>
2025-04-29 12:39:30 +0200

    Add deck,shuffle and draw routes

--- app/src/Controller/CardController.php
+++ app/src/Controller/CardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Card\Card;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +14,37 @@ class CardController extends AbstractController
 {
     #[Route("/card", name: "card")]
     public function card(): Response
+    {   
+        $card = new Card('32319');
+        $data = [
+            'card' => $card->getValue(),
+
+        ];
+        return $this->render('card.html.twig', $data);
+    }
+
+    #[Route("/card/deck", name: "card_deck")]
+    public function card_deck(): Response
+    {   
+        $data = [
+            'card' => null,
+
+        ];
+        return $this->render('card.html.twig', $data);
+    }
+
+    #[Route("/card/deck/shuffle", name: "card_deck_shuffle")]
+    public function card_deck_shuffle(): Response
+    {   
+        $data = [
+            'card' => null,
+
+        ];
+        return $this->render('card.html.twig', $data);
+    }
+
+    #[Route("/card/deck/draw", name: "card_deck_draw")]
+    public function card_deck_draw(): Response
     {   
         $data = [
             'card' => null,
