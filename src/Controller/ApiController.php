851211f9b3232dbe629f70886e41ff1b213785e3
Elis Sandklo <elsd23@student.bth.se>
2025-05-05 23:11:52 +0200

    Refactor api controller and template

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -18,6 +18,28 @@ class ApiController extends AbstractController
                     'url' => '/api/quote',
                     'description' => 'Hämtar ett slumpmässigt citat, dagens datum och timestamp.',
                 ],
+                [
+                    'request' => 'GET',
+                    'url' => '/api/deck',
+                    'description' => 'Hämtar kortleken i sessionen och sorterar den.',
+                ],
+                [
+                    'request' => 'POST',
+                    'url' => '/api/deck/shuffle',
+                    'description' => 'Blandar kortleken.',
+                    'redirect' => '/form/api/deck/shuffle', // Needs a form to be submitted.
+                ],
+                [
+                    'request' => 'POST',
+                    'url' => '/api/deck/draw/:number',
+                    'description' => 'Drar ett x antal (:number) kort från kortleken. Om inget antal anges dras ett kort.',
+                    'redirect' => '/form/api/deck/draw', // needs a form to be submitted.
+                ],
+                [
+                    'request' => 'GET',
+                    'url' => '/api/deck/reset',
+                    'description' => 'Återställer kortleken till en ny sorterad kortlek.',
+                ],
             ],
         ];
         return $this->render('api.html.twig', $data);
