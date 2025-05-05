57ffef24677cc72edde8ca75474aaadefdbe32c5
Elis Sandklo <elsd23@student.bth.se>
2025-05-05 20:31:56 +0200

    Add card route functionality
    Routes card/deck, card/deck/shuffle, card/deck/draw works as intended now. Also added private helper functions to save and check if deck is saved in session

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Card\Card;
+use App\Card\CardGraphic;
+use App\Card\DeckOfCards;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,41 +16,93 @@ class CardController extends AbstractController
     #[Route("/card", name: "card")]
     public function card(): Response
     {
-        $card = new Card('32319');
-        $data = [
-            'card' => $card->getValue(),
-
-        ];
-        return $this->render('card.html.twig', $data);
+        return $this->render('card.html.twig');
     }
 
     #[Route("/card/deck", name: "card_deck")]
-    public function card_deck(): Response
+    public function card_deck(SessionInterface $session): Response
     {
+        $this->ensureDeckExists($session);
+        $deck = new DeckOfCards(json_decode($session->get('deck'), true));
         $data = [
-            'card' => null,
+            'deck' => $deck,
+            'amountOfCards' => $deck->getAmountOfCards(),
 
         ];
-        return $this->render('card.html.twig', $data);
+        return $this->render('card_deck.html.twig', $data);
     }
 
     #[Route("/card/deck/shuffle", name: "card_deck_shuffle")]
-    public function card_deck_shuffle(): Response
+    public function card_deck_shuffle(SessionInterface $session): Response
     {
+        $this->ensureDeckExists($session);
+        $deck = new DeckOfCards(json_decode($session->get('deck'), true));
+        $deck->shuffle();
+        $this->addFlash(
+            'notice',
+            'Kortleken har sorterats.'
+        );
+        $this->saveDeckToSession($session, $deck);
         $data = [
-            'card' => null,
+            'deck' => $deck,
+            'amountOfCards' => $deck->getAmountOfCards(),
 
         ];
-        return $this->render('card.html.twig', $data);
+        return $this->render('card_deck.html.twig', $data);
     }
 
     #[Route("/card/deck/draw", name: "card_deck_draw")]
-    public function card_deck_draw(): Response
+    public function card_deck_draw(SessionInterface $session): Response
     {
+        $this->ensureDeckExists($session);
+        $deck = new DeckOfCards(json_decode($session->get('deck'), true));
+        $card = $deck->drawCard();
+        if ($card === null) {
+            $this->addFlash(
+                'warning',
+                'Inga kort kvar i kortleken.'
+            );
+        }
+
+        $this->saveDeckToSession($session, $deck);
+        $deck2 = new DeckOfCards([['value' => $card->getValue(), 'suit' => $card->getSuit()]]);
+
+        $this->addFlash(
+            'notice',
+            'Drog kortet: ' . $card->getValue() . ' av ' . $card->getSuit()
+        );
         $data = [
-            'card' => null,
+            'deck' => $deck2,
+            'amountOfCards' => $deck->getAmountOfCards(),
 
         ];
-        return $this->render('card.html.twig', $data);
+        return $this->render('card_deck.html.twig', $data);
+    }
+
+    /**
+     * Helper method checking if deck exists in session.
+     */
+    private function ensureDeckExists(SessionInterface $session): void
+    {
+        if (!$session->has('deck')) {
+            $deck = new DeckOfCards();
+            $session->set('deck', $deck->getJSONDeck());
+            $this->addFlash(
+                'warning',
+                'Inget deck fanns sparat, skapade ett nytt.'
+            );
+        }
+    }
+
+    /**
+     * Helper method to save the deck to the session.
+     */
+    private function saveDeckToSession(SessionInterface $session, DeckOfCards $deck): void
+    {
+        $session->set('deck', $deck->getJSONDeck());
+        $this->addFlash(
+            'notice',
+            'Kortleken har sparats i sessionen.'
+        );
     }
 }
