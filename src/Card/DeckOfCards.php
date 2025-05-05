69f55aece7fcdc6de69b205547ca96cd2caef80d
Elis Sandklo <elsd23@student.bth.se>
2025-05-05 20:28:43 +0200

    Adding methods to deckofcards class

--- src/Card/DeckOfCards.php
+++ src/Card/DeckOfCards.php
@@ -7,7 +7,21 @@ Class DeckOfCards
     /** @var Card[] */
     private array $cards;
 
-    public function __construct()
+    public function __construct(array $cardsJSON = null)
+    {
+        if ($cardsJSON !== null) {
+            foreach ($cardsJSON as $card) {
+                if (!isset($card['value']) || !isset($card['suit'])) {
+                    throw new \InvalidArgumentException('Invalid card data.');
+                }
+                $this->cards[] = new CardGraphic($card['value'], $card['suit']);
+            }
+        } else {
+            $this->initializeDeck();
+        }
+    }
+
+    private function initializeDeck(): void
     {
         $this->cards = [];
         foreach (Card::getSuits() as $suit) {
@@ -17,9 +31,27 @@ Class DeckOfCards
         }
     }
 
-    public function getDeck(): array
+    public function getAmountOfCards(): int
+    {
+        return count($this->cards);
+    }
+
+    public function getCards(): array
     {
         return $this->cards;
+    }
+
+    public function shuffle(): void
+    {
+        shuffle($this->cards);
+    }
+
+    public function drawCard(): ?CardGraphic
+    {
+        if (empty($this->cards)) {
+            return null;
+        }
+        return array_pop($this->cards);
     }
 
     public function getJSONDeck(): string
