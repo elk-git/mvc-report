4c7bd31b39acdd9cb1ddb54152b475d31ac139e1
Elis Sandklo <elsd23@student.bth.se>
2025-05-15 14:42:58 +0200

    Init kmom03 pages and templates

---
+++ src/Controller/GameController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    #[Route("/game", name: "game")]
+    public function game(): Response
+    {
+        return $this->render('game.html.twig');
+    }
+
+    #[Route("/game/doc", name: "game/doc")]
+    public function game_doc(): Response
+    {
+        return $this->render('game_doc.html.twig');
+    }
+}
