a6471494e608187e38e71f883d5979cdc1f84658
Elis Sandklo <elsd23@student.bth.se>
2025-06-07 21:49:02 +0200

    Testing scrutinizer

--- tests/config.php
+++ tests/config.php
@@ -4,9 +4,8 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-// Phpstan gets funky med detta.
-// if (method_exists(Dotenv::class, 'bootEnv')) {
-//   (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-// }
-
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+} 
