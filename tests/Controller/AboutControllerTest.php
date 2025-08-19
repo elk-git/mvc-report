ecb6d46945f799ba9bbc1db0c0f67fd480290048
Elis Sandklo <elsd23@student.bth.se>
2025-08-19 17:45:20 +0200

    Fix manifest unittest problem

--- tests/Controller/AboutControllerTest.php
+++ tests/Controller/AboutControllerTest.php
@@ -5,6 +5,11 @@ namespace App\Tests\Controller;
 use App\Controller\AboutController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
+
+
 /**
  * @covers \App\Controller\AboutController
  */
@@ -22,9 +27,18 @@ class AboutControllerTest extends WebTestCase
     public function testAboutControllerDirectCall(): void
     {
         self::bootKernel();
+        $container = static::getContainer();
+
         $controller = new AboutController();
-        $controller->setContainer(static::getContainer());
+        $controller->setContainer($container);
+
+        // Mocka Twig för att undvika manifest.json-problemet
+        $twig = $container->get('twig');
+        $twig->addFunction(new TwigFunction('encore_entry_link_tags', fn($entry) => ''));
+        $twig->addFunction(new TwigFunction('encore_entry_script_tags', fn($entry) => ''));
+
         $response = $controller->about();
+
         $this->assertSame(200, $response->getStatusCode());
         $this->assertNotEmpty($response->getContent());
     }
